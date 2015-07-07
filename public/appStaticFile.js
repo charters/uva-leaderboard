60ec482e3437891e5bc63e9926c9e21c0036e2b9
charters <aac8mf@virginia.edu>
2015-07-07 00:38:15 -0400

    Fiddling diddling

--- public/appStaticFile.js
+++ public/appStaticFile.js
@@ -4,17 +4,17 @@ leaderboardApp.controller('leaderboardCtrl', function($scope, $http){
 
 	$scope.update = function() {
 		$http.get('/api/scrape')
-					.success(function(data){
-						$scope.leaders = data;
-						console.log('All users updated successfully.');
-					})
-					.error(function(data){
-						console.log('Error: ' + data);
-					})
-		$http.get('/api/getAll')
-		.then(function(res){
-			$scope.leaders = res.data;
+		.success(function(data){
+			console.log('All users updated successfully.');
+			$http.get('/api/getAll')
+			.then(function(res){
+				$scope.leaders = res.data;
+			})
 		})
+		.error(function(data){
+			console.log('Error: ' + data);
+		})
+
 	}
 	$scope.initialize = function(){
 		$http.get('/api/getAll')
